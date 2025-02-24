01204f6828723a44571bcdf90636f8572ea63f86
mintuchel <max3627@naver.com>
2025-02-24 18:19:10 +0900

    add: response entity

--- src/main/java/enstudy/signup/domain/member/api/MemberController.java
+++ src/main/java/enstudy/signup/domain/member/api/MemberController.java
@@ -4,10 +4,15 @@ import enstudy.signup.domain.member.dto.request.LoginRequest;
 import enstudy.signup.domain.member.dto.request.SignUpRequest;
 import enstudy.signup.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// @RestController가 @ResponseBody를 포함하므로 리턴되는 객체가 자동으로 JSON으로 변환되며,
+// 응답의 Content-Type이 기본적으로 application/json으로 설정됨
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +21,29 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public int signup(SignUpRequest signUpRequest) {
-        return memberService.signUp(signUpRequest);
+    public ResponseEntity<Integer> signup(SignUpRequest signUpRequest) {
+        int id = memberService.signUp(signUpRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(id);
     }
 
     @GetMapping("/checkIfEmailAvailable")
-    public boolean checkIfEmailAvailable(String email) {
-        return memberService.checkIfEmailAvailable(email);
+    public ResponseEntity<Boolean> checkIfEmailAvailable(String email) {
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(memberService.checkIfEmailAvailable(email));
     }
 
     @PostMapping("/login")
-    public void login(LoginRequest loginRequest) {
-        memberService.login(loginRequest);
+    public ResponseEntity<Void> login(LoginRequest loginRequest) {
+        boolean isValidLogin = memberService.login(loginRequest);
+
+        if(isValidLogin) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
