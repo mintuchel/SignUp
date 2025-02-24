5637c8d7879801a98f20aa3c4468cbf1fe2f4c2f
mintuchel <max3627@naver.com>
2025-02-24 17:31:18 +0900

    add: email check requestdto

--- src/main/java/enstudy/signup/domain/member/dto/request/DuplicateEmailRequest.java
+++ src/main/java/enstudy/signup/domain/member/dto/request/DuplicateEmailRequest.java
@@ -3,11 +3,10 @@ package enstudy.signup.domain.member.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequest(
+// @Email 로 이메일 형식 검증
+// @NotBlank 로 빈 값 방지
+public record DuplicateEmailRequest(
         @Email(message = "유효한 이메일 형식을 입력하세요.")
         @NotBlank(message = "이메일은 필수 입력 값입니다.")
-        String email,
-
-        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-        String password
+        String email
 ) { }
