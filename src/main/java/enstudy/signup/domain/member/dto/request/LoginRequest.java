f1d1d0d1e7005ed7bf5a0c749458c5613f21c18d
mintuchel <max3627@naver.com>
2025-02-24 18:28:06 +0900

    add: dto validation

--- src/main/java/enstudy/signup/domain/member/dto/request/LoginRequest.java
+++ src/main/java/enstudy/signup/domain/member/dto/request/LoginRequest.java
@@ -7,7 +7,6 @@ public record LoginRequest(
         @Email(message = "유효한 이메일 형식을 입력하세요.")
         @NotBlank(message = "이메일은 필수 입력 값입니다.")
         String email,
-
         @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
         String password
 ) { }
--- src/main/java/enstudy/signup/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/enstudy/signup/domain/member/dto/request/SignUpRequest.java
@@ -1,8 +1,16 @@
 package enstudy.signup.domain.member.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record SignUpRequest(
-    String email,
-    String password,
-    String username,
-    String address
+        @Email(message = "유효한 이메일 형식을 입력하세요.")
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        String email,
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        String password,
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+        String username,
+        @NotBlank(message = "주소는 필수 입력 값입니다")
+        String address
 ) { }
