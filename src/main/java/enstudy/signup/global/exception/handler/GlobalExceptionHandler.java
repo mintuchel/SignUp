a1395591d3bf1a01a8c0fc3e24b4f8789cd042f8
mintuchel <max3627@naver.com>
2025-03-02 14:01:23 +0900

    update: for jakarta validation responses

--- src/main/java/enstudy/signup/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/enstudy/signup/global/exception/handler/GlobalExceptionHandler.java
@@ -1,17 +1,36 @@
 package enstudy.signup.global.exception.handler;
 
 import enstudy.signup.global.exception.exception.UserException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
     @ExceptionHandler(value = UserException.class)
     protected ResponseEntity<String> handleUserException(UserException e) {
         return ResponseEntity
                 .status(e.getUserErrorCode().getHttpStatus())
-                .body(e.getMessage());
+                .body(e.getUserErrorCode().getMessage());
+    }
+
+    // Validation 관련 에러 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
     }
 }
