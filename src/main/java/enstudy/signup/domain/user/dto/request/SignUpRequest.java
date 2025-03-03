d1ccc7db233303504b15f7fe1dcac4b505dff023
mintuchel <max3627@naver.com>
2025-03-03 22:48:59 +0900

    add: comment

--- src/main/java/enstudy/signup/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/enstudy/signup/domain/user/dto/request/SignUpRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
+        // 내부적인 정규식으로 통해 email 형식인지 판단
         @Email(message = "유효한 이메일 형식을 입력하세요.")
         @NotBlank(message = "이메일은 필수 입력 값입니다.")
         @Size(message = "이메일은 30자를 넘을 수 없습니다.", max = 30)
@@ -22,6 +23,7 @@ public record SignUpRequest(
         @Size(message = "도로명주소는 30자를 넘을 수 없습니다.", max = 50)
         String streetAddress,
 
+        // 상세주소는 빈값이여도 된다고 한다.. 대원피셜
         @Size(message = "상세주소는 30자를 넘을 수 없습니다.", max = 50)
         String detailAddress
 ) { }
